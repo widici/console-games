74302557ef8112dca2f40bbcaa8d086eb0423f47
widici <vidar.otto.johannessen@gmail.com>
2022-11-17 21:34:51 +0100

    Added initialize and hit functions to Throw class

--- bullseye.rb
+++ bullseye.rb
@@ -5,5 +5,29 @@ class Player
         @name = name
         @score = 0
     end
-
 end
+
+
+class Throw
+    def initialize()
+        $fast = [0.65, 0.55, 0.5, 0.5]
+        $controlled = [0.99, 0.77, 0.43, 0.01]
+        $underarm = [0.95, 0.75, 0.45, 0.05]
+        @points = [40, 30, 20, 10, 0]
+    end
+
+    def hit(array)
+        random = rand
+        array.each_with_index do |point, number|
+            if random >= point
+                place = array.find_index(point)
+                return @points[place]
+            elsif number == 3
+                return 4
+            end
+        end
+    end
+end
+
+throw = Throw.new()
+puts throw.hit($fast)
